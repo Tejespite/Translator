2833f1750fca850ddda64b89ac1c25a4185bfd00
Mohay Henrik <mohay.henrik@gmail.com>
2025-05-30 22:05:52 +0200

    ok

--- src/server/server.cjs
+++ src/server/server.cjs
@@ -11,6 +11,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://latin-hu-translator.netlify.app");
+  res.header("Access-Control-Allow-Origin", "https://latin-dictionary.netlify.app/");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
